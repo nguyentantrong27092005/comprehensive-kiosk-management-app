e106fd0c3c00aa6c20b6ff988bece00b310a60c7
Thien Diep <thiendb0103@gmail.com>
2025-02-24 20:18:40 +0700

    Add script sql cho một số màn hình phía Kiosk App

---
+++ sql/script_for_frames/cart_frame.sql
@@ -0,0 +1,48 @@
+/*Check xem voucher người dùng có phải voucher giảm giá không. Nếu đúng thì kiểm tra tiếp voucher đó đã được sd chưa. Nếu chưa thì câu query sẽ trả kết quả*/
+SELECT ID --ID của voucher
+	  ,EvoucherGiamGiaID --ID của chương trình giảm giá
+FROM evouchergiamgialist
+WHERE Value = {Mã voucher người dùng nhập}
+AND IsUsed = False --Kiểm tra voucher đã sử dụng chưa
+LIMIT 1 --Thường chỉ khớp với 1 mã voucher duy nhất, bỏ qua các trường hợp 1 mã có nhiều ID trong bảng evouchergiamgialist
+
+/*Nếu check ra đúng là voucher giảm giá, kiểm tra xem chương trình còn hiệu lực không. Nếu có thì câu query sẽ trả ra kết quả chi tiết chương trình của voucher. 
+Sử dụng dữ liệu để áp giảm giá cho tổng đơn hàng.*/
+SELECT evgg.ID
+	  ,evgg.Discount --Giá trị giảm
+	  ,evgg.IsPercent --Flag giảm theo phần trăm
+FROM evouchergiamgia evgg
+INNER JOIN evoucher ev
+ON ev.ID = evgg.EVoucherID
+WHERE evgg.ID = {EvoucherGiamGiaID query được ở câu trên}
+AND ev.IsEffective = True --Chương trình còn hiệu lực
+
+
+/*Check xem voucher người dùng có phải voucher tặng món và đã qua sử dụng chưa. Nếu đúng thì câu query sẽ trả kết quả. 
+Nếu không thì hiện thông báo trên UI evoucher không hợp lệ, hoặc đã bị sử dụng.*/
+SELECT ID
+	  ,EVoucherTangMonID
+FROM evouchertangmonlist
+WHERE Value = {Mã voucher người dùng nhập} 
+AND IsUsed = False --Kiểm tra voucher đã sử dụng chưa
+
+/*Nếu voucher người dùng là voucher tặng món, sẽ kiểm tra thêm chương trình còn hiệu lực không. Nếu còn hiệu lực thì giỏ hàng sẽ tự động add thông tin sản phẩm được 
+tặng vào giỏ hàng theo số lượng đã được setup trong db. Sử dụng câu query sau để lấy dữ liệu món tặng. Nếu query không trả lại kq thì hiện thông báo trên UI evoucher đã hết hạn.*/
+SELECT fi.ID
+	  ,fi.Name
+	  ,fi.ImageURL
+	  ,fih.Price
+	  ,0 AS DiscountedPrice --Tặng món nên giá sau giảm là 0đ
+	  ,evtm.ID AS EvoucherTangMonID
+	  ,evtm.Amount --Số lượng món được tặng
+FROM evouchertangmon evtm
+INNER JOIN evoucher ev
+ON ev.ID = evtm.EVoucherID
+INNER JOIN fooditem fi
+ON fi.ID = evtm.FoodItemID
+INNER JOIN fooditem_history fih
+ON fi.ID = fih.FoodItemID
+WHERE evtm.ID = {EVoucherTangMonID query được ở câu trên}
+AND ev.IsEffective = TRUE --chương trình còn hiệu lực không
+AND fih.IsEffective = TRUE
+

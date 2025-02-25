b68ea3e47a0406a453778543319dc386cf00e1ed
= <=>
2025-02-25 20:53:58 +0700

    Add a helper file My SQL

--- common/ex.py
+++ common/ex.py
@@ -1,12 +1,42 @@
-import mysql.connector
+import pymysql
 
 def fetch_data(user, password, query, host="34.101.167.101", database="test_db"):
+    """
+    Truy vấn dữ liệu từ cơ sở dữ liệu.
+
+    Ví dụ sử dụng:
+    --------------
+    query = "SELECT * FROM fooditem;"
+    data = fetch_data("dev", "12345678x@X", query)
+
+    Nếu có dữ liệu, sẽ hiển thị như sau:
+    -------------------------------------
+    Dữ liệu từ bảng fooditem:
+    (row1_data)
+    (row2_data)
+    ...
+
+    Tham số:
+    --------
+    user : str
+        Tên đăng nhập vào cơ sở dữ liệu.
+    password : str
+        Mật khẩu đăng nhập.
+    query : str
+        Câu lệnh SQL cần thực thi.
+
+    Trả về:
+    -------
+    list
+        Danh sách các dòng dữ liệu từ truy vấn.
+    """
     try:
-        conn = mysql.connector.connect(
+        conn = pymysql.connect(
             host=host,
             user=user,
             password=password,
-            database=database
+            database=database,
+            cursorclass=pymysql.cursors.Cursor  # Dùng Cursor để lấy dữ liệu dạng tuple
         )
         cursor = conn.cursor()
 
@@ -18,15 +48,7 @@ def fetch_data(user, password, query, host="34.101.167.101", database="test_db")
 
         return result
 
-    except mysql.connector.Error as err:
+    except pymysql.MySQLError as err:
         print(f"Lỗi kết nối: {err}")
         return None
 
-# Truy vấn để lấy dữ liệu từ bảng fooditem
-query = "SELECT * FROM fooditem;"
-data = fetch_data("dev", "12345678x@X", query)
-
-if data:
-    print("Dữ liệu từ bảng fooditem:")
-    for row in data:
-        print(row)

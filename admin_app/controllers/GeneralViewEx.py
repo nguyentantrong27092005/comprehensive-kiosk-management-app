9d9bbe0f0426c8355cfc446ff75de00c0cd9e4c4
NgocMypzg <mypn23416@st.uel.edu.vn>
2025-03-20 16:03:54 +0700

    Chinh sua lai tinh nang mac dinh 7 ngay gan nhat va fix loi cap nhap vi tri cua calendar khi resize

--- admin_app/controllers/GeneralViewEx.py
+++ admin_app/controllers/GeneralViewEx.py
@@ -1,25 +1,19 @@
-import sys
-
 from PyQt6 import QtCore, QtWidgets
 from PyQt6.QtGui import QTextCharFormat, QBrush, QColor
 from PyQt6.QtCore import QDate
 
-
-
 from admin_app.views.GeneralView import GeneralView
-
 
 class GeneralViewEx(GeneralView):
     def __init__(self):
         super().__init__()
-        self.lineEditDate.setPlaceholderText("Chọn thời gian")
         self.lineEditDate.setReadOnly(True)
-        self.selected_dates = []
+        self.selected_dates = [QDate.currentDate().addDays(-7), QDate.currentDate()]
+        """self.selected_date chứa các ngày (dạng QDate) được chọn / ngày mặc định (7 ngày gần nhất nếu người dùng không chọn). Có thể chuyển về dạng string để query trên sql"""
 
         self.calendarFrame = QtWidgets.QFrame(self)
         self.calendarFrame.setFrameShape(QtWidgets.QFrame.Shape.StyledPanel)
         self.calendarFrame.setFrameShadow(QtWidgets.QFrame.Shadow.Raised)
-        self.calendarFrame.setParent(self)
 
         self.calendarLayout = QtWidgets.QGridLayout(self.calendarFrame)
         self.calendarFrame.setVisible(False)
@@ -27,6 +21,8 @@ class GeneralViewEx(GeneralView):
         # Calendar widget
         self.calendar = QtWidgets.QCalendarWidget()
         self.calendar.setMaximumDate(QDate.currentDate())
+        dates = "-".join([date.toString("dd/MM/yyyy") for date in self.selected_dates])
+        self.lineEditDate.setText(str(dates))
         self.calendar.setVerticalHeaderFormat(QtWidgets.QCalendarWidget.VerticalHeaderFormat.NoVerticalHeader)  # Ẩn số thứ tự
         self.calendar.setStyleSheet("""
                             QCalendarWidget {
@@ -151,25 +147,38 @@ class GeneralViewEx(GeneralView):
         self.calendarLayout.addWidget(self.select_button, 1, 1, 1, 1)
         self.calendarFrame.setFixedSize(300, 315)
         self.calendarLayout.setContentsMargins(0, 0, 0, 0)
-
         self.signalandslot()
-
 
     def signalandslot(self):
         self.calendar.clicked.connect(self.handle_date_selection)
         self.cancel_button.clicked.connect(self.hideCalendar)
-        self.lineEditDate.mousePressEvent = lambda event: self.showCalendar(event, self.lineEditDate)
-        self.select_button.clicked.connect(lambda lineedit: self.showSelectedDate(self.lineEditDate))
-    def showSelectedDate(self, lineEditDate):
+        self.lineEditDate.mousePressEvent =  lambda event:self.showCalendar()
+        self.select_button.clicked.connect(self.showSelectedDate)
+    def showSelectedDate(self):
         self.calendarFrame.setVisible(False)
         dates = "-".join([date.toString("dd/MM/yyyy") for date in self.selected_dates])
-        lineEditDate.setText(str(dates))
+        self.lineEditDate.setText(str(dates))
 
     def hideCalendar(self):
+        """Nếu cancel thì sẽ trở về ngày mặc định"""
         self.calendarFrame.setVisible(False)
-        self.selected_dates =[]
+        self.calendar.setSelectedDate(QDate.currentDate())
+        self.calendar.update()
+        date_str = self.lineEditDate.text()
+        dates = date_str.split('-')
+        start_date_str = dates[0] if len(dates) > 0 else ""
+        end_date_str = dates[1] if len(dates) > 1 else start_date_str
 
-
+        start_date = QDate.fromString(start_date_str, "dd/MM/yyyy")
+        end_date = QDate.fromString(end_date_str, "dd/MM/yyyy")
+        if 0 < len(self.selected_dates):
+            if start_date and end_date and start_date!= end_date:
+                self.selected_dates = []
+                self.selected_dates.append(start_date)
+                self.selected_dates.append(end_date)
+            elif start_date and end_date and start_date == end_date and start_date not in self.selected_dates:
+                self.selected_dates.append(start_date)
+        self.selected_dates.sort()
     def handle_date_selection(self, date):
         """Xử lý khi người dùng chọn ngày"""
         if date in self.selected_dates:
@@ -178,21 +187,21 @@ class GeneralViewEx(GeneralView):
             if len(self.selected_dates) < 2:
                 self.selected_dates.append(date)
             else:
-                self.selected_dates = [date]  # Reset nếu chọn ngày thứ 3
-
+                self.selected_dates = [date]
         self.selected_dates.sort()
         self.update_highlighted_dates()
 
     def update_highlighted_dates(self):
         """Thêm màu đỏ ngày chọn"""
         self.calendar.setDateTextFormat(QDate(), QTextCharFormat())
-
+        print(self.selected_dates)
         if len(self.selected_dates) == 1:
             self.highlight_dates([self.selected_dates[0]], [])
         elif len(self.selected_dates) == 2:
             start, end = self.selected_dates
             range_dates = [start.addDays(i) for i in range(1, start.daysTo(end))] if start < end else []
             self.highlight_dates([start, end], range_dates)
+
 
     def highlight_dates(self, selected_dates, range_dates):
         """Thêm màu dải ngày"""
@@ -205,30 +214,23 @@ class GeneralViewEx(GeneralView):
         range_fmt.setForeground(QBrush(QColor("#bd1906")))
 
         for date in selected_dates:
+            print("Ngày",date)
             self.calendar.setDateTextFormat(date, selected_fmt)
-
         for date in range_dates:
             self.calendar.setDateTextFormat(date, range_fmt)
 
     def update_calendar(self):
         """Cập nhật lịch khi chọn tháng/năm khác"""
         selected_date = self.calendar.selectedDate()
-        # selected_year = int(self.year_selector.currentText())
         selected_year = selected_date.year()
         print(selected_year)
         selected_month = self.month_selector.currentIndex() + 1
         self.calendar.setCurrentPage(selected_year, selected_month)
-        # Danh sách ngày đã chọn
-    def showCalendar(self, event, lineEdit):
-        pos_lineEdit = lineEdit.mapToGlobal(lineEdit.rect().bottomLeft())
-        # Tính toán vị trí hiển thị lịch
-        cal_width = self.calendarFrame.width()
-        cal_height = self.calendarFrame.height()
-        screen_rect = QtWidgets.QApplication.primaryScreen().geometry()
-        # Nếu lịch bị lấp bên phải màn hình -> đẩy sang trái
-        if pos_lineEdit.x() + cal_width > screen_rect.right():
-            pos_lineEdit.setX(screen_rect.right() - cal_width - 85)
-        self.calendarFrame.move(pos_lineEdit)
+
+    def showCalendar(self):
+        pos_lineEdit = self.frameContent.mapToParent(self.frameContent.rect().topLeft())
+        self.calendarFrame.move(pos_lineEdit.x(), pos_lineEdit.y() + 50)
+        self.update_highlighted_dates()
         self.calendarFrame.show()
 
 if __name__ == "__main__":
